ea340c48c8c70d2748e7f859da48bf84877bed15
Ken Fishkin <ken.fishkin@gmail.com>
2021-03-28 22:59:43 -0700

    added github link to about page

--- src/AboutPage.js
+++ src/AboutPage.js
@@ -6,7 +6,7 @@ class AboutPage extends React.Component {
     render() {
         return (<div>
             <p>Version {VERSION}.</p>
-            <p>Project is on Github at (somewhere)</p>
+            <p>Project is on Github at https://github.com/kfishkin/brago</p>
         </div>);
     }
 }
